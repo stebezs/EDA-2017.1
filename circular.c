47a435059d854e8f9a4c1fe564bfe963b76a5390
Stefania Bezerra da Silva <stefaniabezerra92@gmail.com>
2017-04-27 16:15:02 -0300

    Deus é top

---
+++ circular.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <<stdlib.h>>
+
+struct cadastro {
+  int codigo;
+  int primeiroNOme;
+  int nomeFamilia;
+  int idade;
+} ;
+
+typedef struct Tno{
+  
+}
+
+
+// Liste na tela todos os registros por ordem crescente de código;
+
+//Liste na tela todos os registros por ordem decrescente de código;
+
+// Leia os dados de um novo registro e o inclua na lista (mantendo-a ordenada por código). Lembre-se que não pode haver códigos repetidos.
+
+// Exclua o registro de um código informado pelo usuário;
+
+// Liste o registro de um código informado pelo usuário;
+
+// Altere o registro de um código informado pelo usuário
+
+
+int main() {
+
+  return 0;
+}
