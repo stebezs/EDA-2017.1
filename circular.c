7575af6f85a9fc1ee3bea686307cdacfb5e1f9f3
Stefania Bezerra da Silva <stefaniabezerra92@gmail.com>
2017-04-28 22:08:59 -0300

    add

--- circular.c
+++ circular.c
@@ -1,21 +1,55 @@
 #include <stdio.h>
-#include <<stdlib.h>>
+#include <stdlib.h>
 
-struct cadastro {
+struct Node {
   int codigo;
-  int primeiroNOme;
-  int nomeFamilia;
+  char primeiroNOme;
+  char nomeFamilia;
   int idade;
+  struct Node *proximo;
+  struct Node *anterior;
 } ;
 
-typedef struct Tno{
-  
+typedef struct Tno {
+  int info;
+  struct Tno *prox;
+  struct Node *head;
+  struct Node *tail;
+} ;
+
+void iniciaLista (Tno *listaCadastro){
+  prox->NULL;
+  anterior->NULL;
+  tamanhoLista = 0;
 }
 
 
 // Liste na tela todos os registros por ordem crescente de código;
+void listar (struct Tno *ptlista) {
 
-//Liste na tela todos os registros por ordem decrescente de código;
+     int i = 0;
+     struct Tno *pont;
+
+     pont = ptlista->head;
+     while (pont != ptlista) {
+           printf("Elemento %d = %d\n", i++, pont->info);
+           pont = pont->tail;
+     }
+}
+
+// Liste na tela todos os registros por ordem decrescente de código;
+void listar (struct Tno *ptlista) {
+
+     int i = 0;
+     struct Tno *pont;
+
+     pont = ptlista->head;
+     while (pont != ptlista) {
+           printf("Elemento %d = %d\n", i++, pont->info);
+           pont = pont->;
+     }
+}
+
 
 // Leia os dados de um novo registro e o inclua na lista (mantendo-a ordenada por código). Lembre-se que não pode haver códigos repetidos.
 
